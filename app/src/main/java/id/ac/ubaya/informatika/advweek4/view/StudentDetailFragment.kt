47983bd09992406cb6e067aacbfc37108af5541d
dbaguskrsina <dbaguskrisna@gmail.com>
2021-03-09 13:20:44 +0700

    create project

---
+++ app/src/main/java/id/ac/ubaya/informatika/advweek4/view/StudentDetailFragment.kt
@@ -0,0 +1,26 @@
+package id.ac.ubaya.informatika.advweek4.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import id.ac.ubaya.informatika.advweek4.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+class StudentDetailFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_detail, container, false)
+    }
+
+
+
+}
